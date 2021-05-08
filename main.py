73fa8968f72efa660631f5f74044a7c8ff6cdfa1
Nerubes <gurov.no@phystech.edu>
2021-05-08 15:11:13 +0300

    flask

--- main.py
+++ main.py
@@ -16,8 +16,11 @@ def get_message(message):
     :param message: сообщение
     """
     res = generate_message(message.text, message.from_user.id)
-    for i in range(res[0][3]):
-        bot.send_photo(res[i][0], res[i][1], res[i][2])
+    if isinstance(res[0], tuple):
+        for i in range(res[0][3]):
+            bot.send_photo(res[i][0], res[i][1], res[i][2])
+    else:
+        bot.send_photo(res[0], res[1], res[2])
 
 
 def generate_message(message_text, user_id):
@@ -27,78 +30,74 @@ def generate_message(message_text, user_id):
     :param user_id: id пользователя
     """
     commands = message_text.split()
-    ans_to = []
     if commands[0] == "/help" or commands[0] == "/start":
-        ans_to.append((user_id, const.HELP,
-                       "/top random\n" +
-                       "/top [number]\n" +
-                       "/new\n" +
-                       "/random\n" +
-                       "/search [your text]\n" +
-                       "/search_genre_top [детектив/комедия/пародия/мехи/повседневность/романтика,/фантастика/этти]\n" +
-                       "/subscribe\n" +
-                       "/unsubscribe\n", 1))
-        return ans_to
-    if len(commands) == 2 and commands[0] == "/top":
+        return user_id, const.HELP, \
+               ("/top random\n" +
+                "/top [number]\n" +
+                "/new\n" +
+                "/random\n" +
+                "/search [your text]\n" +
+                "/search_genre_top [детектив/комедия/пародия/мехи/повседневность/романтика,/фантастика/этти]\n" +
+                "/subscribe\n" +
+                "/unsubscribe\n"), 1
+    elif len(commands) == 2 and commands[0] == "/top":
         if commands[1] == "random":
             res = parse.parse_top(100)
             answ = res[random.randrange(100)]
-            ans_to.append((user_id, answ[0], answ[1] + ": " + answ[2], 1))
+            return user_id, answ[0], answ[1] + ": " + answ[2], 1
         elif commands[1].isnumeric():
             res = parse.parse_top(int(commands[1]))
-            for top_anim in res:
-                ans_to.append((user_id, top_anim[0], top_anim[1] + ": " + top_anim[2], len(res)))
+            return append_all(res, user_id)
         else:
-            ans_to.append((user_id, const.NOTHING, "Invalid Command. Type /help for all commands.", 1))
-        return ans_to
-    if commands[0] == "/new":
+            return user_id, const.NOTHING, "Invalid Command. Type /help for all commands.", 1
+    elif commands[0] == "/new":
         res = parse.parse_new(5)
-        for new in res:
-            ans_to.append((user_id, new[0], new[1] + ": " + new[2], len(res)))
-        return ans_to
-    if commands[0] == "/random":
+        return append_all(res, user_id)
+    elif commands[0] == "/random":
         res = parse.parse_random()
-        ans_to.append((user_id, res[0], res[1] + ": " + res[2], 1))
-        return ans_to
-    if len(commands) >= 2 and commands[0] == "/search":
+        return user_id, res[0], res[1] + ": " + res[2], 1
+    elif len(commands) >= 2 and commands[0] == "/search":
         ser = make_word(commands, "_")
         res = parse.search(ser)
         if res == []:
-            ans_to.append((user_id, const.NOTHING, "Nothing was found.", 1))
+            return user_id, const.NOTHING, "Nothing was found.", 1
         else:
-            for anim in res:
-                ans_to.append((user_id, anim[0], anim[1] + ": " + anim[2], len(res)))
-        return ans_to
-    if len(commands) >= 2 and commands[0] == "/search_genre_top":
+            return append_all(res, user_id)
+    elif len(commands) >= 2 and commands[0] == "/search_genre_top":
         index = parse.get_index(commands[1])
         if index < 0:
-            ans_to.append((user_id, const.NOTHING, "Invalid genre.", 1))
+            return user_id, const.NOTHING, "Invalid genre.", 1
         else:
             res = parse.search_genre_top(index)
-            for i in res:
-                ans_to.append((user_id, i[0], i[1] + ": " + i[2], len(res)))
-        return ans_to
-    if commands[0] == "/subscribe":
+            return append_all(res, user_id)
+    elif commands[0] == "/subscribe":
         database.subscribe(user_id, const.DATABASE)
         print(user_id)
-        ans_to.append((user_id, const.SUB, "You were subscribed.", 1))
-        return ans_to
-    if commands[0] == "/unsubscribe":
+        return user_id, const.SUB, "You were subscribed.", 1
+    elif commands[0] == "/unsubscribe":
         database.unsubscribe(user_id, const.DATABASE)
-        ans_to.append((user_id, const.UNSUB, "You were unsubscribed.", 1))
-        return ans_to
-    if commands[0] == "//send" and const.ADMIN == user_id:
+        return user_id, const.UNSUB, "You were unsubscribed.", 1
+    elif commands[0] == "//send" and const.ADMIN == user_id:
         ser = make_word(commands, " ")
+        ans_to = []
         for sub in database.get_all_subs(const.DATABASE):
             ans_to.append((sub[0], const.ALL, ser, len(database.get_all_subs(const.DATABASE))))
         return ans_to
-    return user_id, const.NOTHING, "Invalid Command. Type /help for all commands.", 1
+    else:
+        return user_id, const.NOTHING, "Invalid Command. Type /help for all commands.", 1
 
 
 def make_word(list_of_word, space):
     res = ""
     for indx in range(1, len(list_of_word)):
         res = res + list_of_word[indx] + space
+    return res
+
+
+def append_all(info_list, user_id):
+    res = []
+    for info in info_list:
+        res.append((user_id, info[0], info[1] + ": " + info[2], len(info_list)))
     return res
 
 
